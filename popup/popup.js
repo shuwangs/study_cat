c62da3cdc20e5bc2e0b45754a845eb7ab2f034e8
Shu <shuw425@gmail.com>
2025-11-14 14:52:57 -0500

    add resetBtn function, include a coin icon

--- popup/popup.js
+++ popup/popup.js
@@ -15,7 +15,7 @@ const input_sec = document.getElementById("seconds");
 console.log(parseInt(input_min, 10) || 0);
 console.log(parseInt(input_sec, 10) || 0) ;
 
-// Update timerDisplay
+// Update timerDisplay with Input time
 function UpdateDisplayFromInput () {
   const m = parseInt(input_min.value, 10) || 0;
   const s = parseInt(input_sec.value, 10)|| 0;
@@ -23,4 +23,13 @@ function UpdateDisplayFromInput () {
 }
 input_min.addEventListener("input", UpdateDisplayFromInput);
 input_sec.addEventListener("input", UpdateDisplayFromInput);
+
+// Reset function
+function displayReset() {
+  timer_display.textContent = `25:00`
+  input_min.value = "25";
+  input_sec.value = "00";
+}
+const resetBtn = document.getElementById("resetBtn");
+resetBtn.addEventListener("click", displayReset);
 
