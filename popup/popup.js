5cb87efa3e4883edcc994b3dd47fcbf856917190
Shu <shuw425@gmail.com>
2025-11-14 16:20:45 -0500

    implement pause and reset function - pause/reset clear the timeCountDown setinterval function Next: start buton should get starting point time from timer_display not the time_input.

--- popup/popup.js
+++ popup/popup.js
@@ -13,15 +13,18 @@ const input_sec = document.getElementById("seconds");
 const startBtn = document.getElementById("startBtn");
 const pauseBtn = document.getElementById("pauseBtn");
 const resetBtn = document.getElementById("resetBtn");
+const totalCoins = document.getElementById("coinCount");
 
 let isRunning = false;
 let totalSeconds = 0;
 let remainingSeconds = 0;
+let coins = 0;
+let timeCountDown;
 /*
 * Get input time and minutes
 */
 function getInputTime(inputTime){
-  const time = parseInt(input_min.value, 10) || 0;
+  const time = parseInt(inputTime, 10) || 0;
   return time;
 }
 
@@ -76,30 +79,69 @@ function startTimer() {
       if (inputArea) {
         inputArea.style.display = "none";
       }
-  
+      // TODOS:
+      // 1. Set time intevals to reduce time 
+      // 2. add coins?
+      timeCountDown = setInterval(() => {
+        remainingSeconds--;
+
+        // display time 
+        displayMins = Math.floor(remainingSeconds / 60)
+        displaySecs = remainingSeconds % 60;
+        timer_display.textContent = `${displayMins.toString().padStart(2, "0")}:${displaySecs.toString().padStart(2, "0")}`
+        
+
+        // Increase total coins
+        if((totalSeconds - remainingSeconds) % 60 === 0 && remainingSeconds > 0) {
+          coins = parseInt(totalCoins.textContent) + 1;
+          
+          totalCoins.textContent = coins.toString();
+        }
+        if(remainingSeconds <= 0) {
+          clearInterval(timeCountDown);
+          isRunning = false;
+          toggleBtns();
+
+        }
+      }, 1000); 
+
     }
 }
 
+// Pause Btn function
+function pauseTimer() {
+  clearInterval(timeCountDown);
+
+  isRunning = false;
+  toggleBtns();
+}
 
 // Reset btn function
 function resetTimer() {
+  clearInterval(timeCountDown);
+
   isRunning = false;
   toggleBtns();
   const inputArea = document.getElementById("timeInput");
-    if (inputArea) {
-      inputArea.style.display = "flex";
-    }
+  if (inputArea) {
+    inputArea.style.display = "flex";
+  }
   timer_display.textContent = `25:00`
   input_min.value = "25";
   input_sec.value = "00";
 }
 
+
+
+
+
 input_min.addEventListener("input", UpdateDisplayFromInput);
 input_sec.addEventListener("input", UpdateDisplayFromInput);
 
+
 // Add EventListener to Start button, reset button
 startBtn.addEventListener("click", startTimer);
-
+pauseBtn.addEventListener("click", pauseTimer);
 resetBtn.addEventListener("click", resetTimer);
 
 // Start btn
