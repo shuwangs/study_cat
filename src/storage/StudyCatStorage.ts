32c8609d62faa51f225fa530de70fe691c9f7ce3
shuwang425 <60482034+shuwang425@users.noreply.github.com>
2025-11-19 17:57:34 -0500

    add structure of load, save, reset function in storage

--- src/storage/StudyCatStorage.ts
+++ src/storage/StudyCatStorage.ts
@@ -6,11 +6,48 @@ import { Mood } from "../models/Mood.js";
 // Load / restore from the storage
 // Save and update to the storage
 
-const DEFAULT_STUDY_CAT_STATE: StudyCatState = {
+const DEFAULT_STATE: StudyCatState = {
   coins: 0,
   currentMood: Mood.SLEEPY,
   blackList: [],
   elapsedTime: 0
 };
 
-export class StudyCatStorage {}
+export class StudyCatStorage  {
+  // load state from a stored object
+  static loadState(): Promise<StudyCatState> {
+    return new Promise((resolve) => {
+      chrome.storage.sync.get(["key"]).then((result) => {
+        const stored = result["key"];
+      
+        if(!stored){
+          resolve(DEFAULT_STATE);
+          return;
+        }
+        
+        const state: StudyCatState = {
+          coins: stored.coins ?? DEFAULT_STATE.coins,
+          currentMood: stored.currentMood ?? DEFAULT_STATE.currentMood,
+          blackList: stored.blackList ?? DEFAULT_STATE.blackList,
+          elapsedTime: stored.elapsedTime ?? DEFAULT_STATE.elapsedTime
+        };
+        resolve(state);
+      });
+    });
+  }
+
+  // save state to a storable object
+  public static saveState(state: StudyCatState): void {
+    chrome.storage.sync.set({ key: state }).then(() => {
+      console.log("Value is set");
+    });
+
+  }
+
+  // reset state to default values
+  public static resetState(): void {
+    chrome.storage.sync.set({ key: DEFAULT_STATE }).then(() => {
+      console.log("State has been reset to default");
+    });
+  }
+};
