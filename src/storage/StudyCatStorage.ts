9caf3c817934fa59e979eb2d8265760a76800ad9
shuwang425 <60482034+shuwang425@users.noreply.github.com>
2025-11-19 11:10:05 -0500

    add todos for StudyCatStorage

--- src/storage/StudyCatStorage.ts
+++ src/storage/StudyCatStorage.ts
@@ -1,6 +1,11 @@
 import {StudyCatState} from "../models/StudyCatState.js";
 import { Mood } from "../models/Mood.js";
 
+// TODOS:
+// Set Default StudyCatState
+// Load / restore from the storage
+// Save and update to the storage
+
 const DEFAULT_STUDY_CAT_STATE: StudyCatState = {
   coins: 0,
   currentMood: Mood.SLEEPY,
